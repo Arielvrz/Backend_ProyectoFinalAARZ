7cee9e223a0667a97b5067c4dba0868383003c79
Rolando Antonio Zelaya Bulnes <20235007@esen.edu.sv>
2026-04-08 21:50:29 -0600

    final commit

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-use L5Swagger\L5SwaggerServiceProvider; // AGREGA ESTA LÍNEA
-
 return [
-    AppServiceProvider::class,
-    L5SwaggerServiceProvider::class, // AHORA QUEDA ASÍ, MÁS LIMPIO
+    App\Providers\AppServiceProvider::class,
+    \L5Swagger\L5SwaggerServiceProvider::class,
 ];
